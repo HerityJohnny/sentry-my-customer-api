5889f00e0b9a838251e7e288fc77305c8a35188f
Tobi Ajibade <herityjohnny14>
2020-07-02 17:57:01 -0700

    Added the forgot password controller and route

---
+++ controllers/forgot-password.js
@@ -0,0 +1,69 @@
+const User = require('../models/user');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+    forgotPassword(req,res) {
+        let { email, newpassword } = req.body;
+        /**
+         * Search if user exist 
+         */
+        User.findOne({email : email})
+        .exec()
+        .then(result => {
+            if(!result) {
+                res.status(401).json({
+                    success : false,
+                    message : "User Not Found",
+                    error : {
+                        code : 401,
+                        message : "User not Found"
+                    }
+                })
+            } else {
+                /**
+                 * if a user is found hash the password and save it to the database
+                 */
+                bcrypt.genSalt(10, (err,salt) => {
+                    bcrypt.hash(newpassword,salt, (err,hash) => {
+                        newpassword = hash;
+                        User.updateOne({email : email}, { $set : {password : newpassword}})
+                        .exec()
+                        .then(docs => {
+                            return res.status(200).json({
+                                status : true,
+                                message : "Password updated successfully",
+                                data : {
+                                    user : {
+                                        id : docs._id,
+                                        email : docs.email,
+                                        phone_number : docs.phone_number
+                                    }
+                                }
+                            })
+                        })
+                        .catch(err => {
+                            res.status(404).json({
+                                success : false,
+                                message : "Failed to update password",
+                                error : {
+                                    code : err.code,
+                                    message : err.message
+                                }
+                            })
+                        })
+                    })
+                })
+            }
+        })
+        .catch(err => {
+            res.status(404).json({
+                success : false,
+                message : "An error occured",
+                error : {
+                    code : err.code,
+                    message : err.message
+                }
+            })
+        })
+    }
+}
---
+++ routes/forgot-password.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const forgetpassControl = require('../controllers/forgot-password');
+const router = express.Router();
+
+router.post('/forgotpassword',forgetpassControl.forgotPassword);
+
+module.exports = router; 
